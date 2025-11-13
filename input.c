317c2bf599755a055e7b543f99b9439d16bc0771
Cezary Kacprzak <cezarykacprzak@MacBook-Pro-Cezary.local>
2025-11-13 20:25:19 +0100

    Program z scanf

---
+++ input.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void) {
+    int liczba;
+
+    printf("Podaj liczbe calkowita: ");
+    scanf("%d", &liczba);
+
+    printf("Wprowadziles: %d\n", liczba);
+
+    return 0;
+}
